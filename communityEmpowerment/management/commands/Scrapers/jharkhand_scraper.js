65dbdad016916ffc6fb40ec1d3a2d89f1aa9ad09
aryan7081 <haryan458@gmail.com>
2025-02-03 12:50:25 +0530

    Updated jharkhand's scraper for more scholarships and structured data.

--- communityEmpowerment/management/commands/Scrapers/jharkhand_scraper.js
+++ communityEmpowerment/management/commands/Scrapers/jharkhand_scraper.js
@@ -23,19 +23,6 @@ const getAllUrls = async function(){
     return values 
 }
 
-
-async function main() {
-    const allResults = []
-    const values = await getAllUrls()
-    await Promise.all(values.map(async (item) => {
-        const res = await get_pdf_link(`https://jharkhand.gov.in/Home/SearchSchemes?department=${item.value}`, item.departmentName)
-        allResults.push(...res)
-    }))
-    const targetDir = path.join(__dirname, '..','..','scrapedData', 'scrapedPdfs');
-    const filePath = path.join(targetDir, 'jharkhandPdf.json');
-    await fs.writeFile(filePath, JSON.stringify(allResults, null, 2))
-}
-main()
 async function get_pdf_link(url, departmentName) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
@@ -59,6 +46,63 @@ async function get_pdf_link(url, departmentName) {
     return resultWithUUID
 
 }
+
+async function scrapeJharkhandScholarships(){
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto("https://ekalyan.cgg.gov.in/")
+    const result = await page.evaluate(()=>{
+        const descriptionList = []
+        const allSchemes = []
+        const preAndPostMatricSchemeNodes = document.querySelectorAll(".schemes .container .row .col-md-4 ul li:not(:nth-child(1))")
+        // const postMatricSchemeNodes = document.querySelectorAll(".container .row :nth-child(4) ul li:not(:nth-child(1))")
+        const descriptionNodes = document.querySelectorAll("#pills-tabContent #pills-home .block-hdnews ul li:not(:last-child)")
+        descriptionNodes.forEach((node)=>{
+            const description = node.textContent
+            descriptionList.push(description)
+        })
+
+        preAndPostMatricSchemeNodes.forEach((node)=>{
+            const title = node.textContent.trim()
+            const scheme = {
+                title,
+                description: [...descriptionList],
+                id: self.crypto.randomUUID(),
+                schemeUrl: "https://ekalyan.cgg.gov.in/"
+            }
+            allSchemes.push(scheme)
+        })
+
+        return allSchemes
+
+        
+    })
+    await browser.close()
+    return result
+}
+
+
+
+
+async function main() {
+    const allResults = []
+    const values = await getAllUrls()
+    await Promise.all(values.map(async (item) => {
+        const res = await get_pdf_link(`https://jharkhand.gov.in/Home/SearchSchemes?department=${item.value}`, item.departmentName)
+        allResults.push(...res)
+    }))
+
+    const jharkhandScholarships = await scrapeJharkhandScholarships()
+    const targetDir = path.join(__dirname, '..','..','scrapedData', 'scrapedPdfs');
+    const filePath = path.join(targetDir, 'jharkhandPdf.json');
+
+    const targetDir2 = path.join(__dirname, '..','..','scrapedData');
+    const filePath2 = path.join(targetDir, 'jharkhand.json');
+
+    await fs.writeFile(filePath, JSON.stringify(allResults, null, 2))
+    await fs.writeFile(filePath2, JSON.stringify(jharkhandScholarships, null, 2))
+}
+main()
 
 
 
